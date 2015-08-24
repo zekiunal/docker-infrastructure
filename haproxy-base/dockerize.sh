a40d638205897d624a760afbbd876e903df2486c
Zeki Unal <zekiunal@gmail.com>
2015-08-25 01:50:21 +0300

    new haproxy

--- haproxy-base/dockerize.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-TAG="monapi/ha-proxy-base"
-IMAGE=$(docker images | grep "$TAG" | awk '{print $3}')
-
-if [[ -z ${IMAGE} ]]; then
-  echo "Building image"
-  docker build --no-cache -t ${TAG} .
-fi
--- haproxy/template.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-/opt/consul/consul-template/consul-template -consul 172.17.0.171:8500 -template "/tmp/haproxy.ctmpl:/etc/haproxy/haproxy.cfg:/usr/sbin/haproxy reload -db -f /etc/haproxy/haproxy.cfg || true"
