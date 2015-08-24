ed53768a174fad619be5e597cdb83d50b9db235e
Zeki Unal <zekiunal@gmail.com>
2015-08-24 22:47:55 +0300

    added consul server.

--- consul-server/run.sh
+++ consul-server/run.sh
@@ -18,5 +18,5 @@ fi
 
 CID=$(docker run -d -p 8300:8300 -p 8301:8301 -p 8301:8301/udp -p 8302:8302 -p 8302:8302/udp -p 8400:8400 -p 8500:8500 -p 8600:8600/udp --name=consul_server ${TAG})
 IP=$(docker inspect -f '{{ .NetworkSettings.IPAddress }}' ${CID})
-echo ${IP}
+echo "Consul Server Runing at " ${IP}
 CONSUL_SERVER_IP=${IP}
