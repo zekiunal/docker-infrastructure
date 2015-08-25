f84635c33ac20b228bb36de94b95ae832f8aa820
Zeki Unal <zekiunal@gmail.com>
2015-08-25 03:21:19 +0300

    fix

--- centos/dockerize.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-TAG="monapi/centos7"
-IMAGE=$(docker images | grep "$TAG" | awk '{print $3}')
-
-if [[ -z ${IMAGE} ]]; then
-  echo "Building image"
-  docker build --no-cache -t ${TAG} .
-fi
--- consul-client/dockerize.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-TAG="monapi/consul_client"
-IMAGE=$(docker images | grep "$TAG" | awk '{print $3}')
-
-if [[ -z ${IMAGE} ]]; then
-  echo "Building image"
-  docker build --no-cache -t ${TAG} .
-fi
