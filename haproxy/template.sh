50d210a7293493c1685093507b603c7650ba4564
Zeki Unal <zekiunal@gmail.com>
2015-08-25 05:27:46 +0300

    fix

--- haproxy/template.sh
+++ haproxy/template.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-/opt/consul/consul-template/consul-template -consul 172.17.0.171:8500 -template "/tmp/haproxy.ctmpl:/etc/haproxy/haproxy.cfg:/usr/sbin/haproxy -db -f /etc/haproxy/haproxy.cfg || true"
+/opt/consul/consul-template/consul-template -consul 172.17.0.171:8500 -template "/tmp/haproxy.ctmpl:/etc/haproxy/haproxy.cfg:kill -9 $(pgrep -f '/usr/sbin/haproxy -db -f /etc') || true"
+
+#/usr/sbin/haproxy reload -db -f /etc/haproxy/haproxy.cfg
+#/usr/sbin/haproxy -f /etc/haproxy/haproxy.cfg -p /var/run/haproxy.pid -sf 6
+#
+#/usr/sbin/haproxy
+#
+#echo $(pgrep -f '/usr/sbin/haproxy -db -f /etc')
+#
+#/usr/sbin/haproxy -sf $(pgrep -f '/usr/sbin/haproxy -db -f /etc')
+#
+#/usr/sbin/haproxy -f haproxy.cfg -p $(</haproxy.pid) -st $(</haproxy.pid)
